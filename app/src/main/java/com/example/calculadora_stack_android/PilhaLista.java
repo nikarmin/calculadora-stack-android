24893b5dd3664381b2ebe19799aed067e8df0f3f
NICOLI FERREIRA <u21689@academico.cotuca.unicamp.br>
2022-09-08 11:46:11 -0300

    socorro 😩

---
+++ app/src/main/java/com/example/calculadora_stack_android/PilhaLista.java
@@ -0,0 +1,70 @@
+package com.example.calculadora_stack_android;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// como faz pra D implementar Comparable?
+public class PilhaLista<D extends Comparable<D>> implements Stack<D>
+{
+    private NoLista<D> topo;
+    private int tamanho;
+
+    public PilhaLista()
+    {
+        topo = null;
+        tamanho = 0;
+    }
+
+    @Override
+    public void empilhar(D dado)
+    {
+        NoLista<D> novoNo = new NoLista<D>(dado, topo);
+        topo = novoNo;
+        tamanho++;
+    }
+
+    @Override
+    public D desempilhar() throws Exception
+    {
+        if (estaVazia())
+            throw new Exception("Underflow da pilha!");
+        D dado = topo.getInfo();
+        topo = topo.getProx();
+        tamanho--;
+        return dado;
+    }
+
+    @Override
+    public D oTopo() throws Exception
+    {
+        if (estaVazia())
+            throw new Exception("Underflow da pilha!");
+        return topo.getInfo();
+    }
+
+    @Override
+    public int getTamanho()
+    {
+        return tamanho;
+    }
+
+    @Override
+    public boolean estaVazia()
+    {
+        return topo == null;
+    }
+
+    @Override
+    public List<D> dadosDaPilha()
+    {
+        List<D> lista = new ArrayList<>();
+        NoLista<D> atual = topo;
+
+        while (atual != null)
+        {
+            lista.add(atual.getInfo());
+            atual = atual.getProx();
+        }
+        return lista;
+    }
+}
---
+++ app/src/main/java/com/example/calculadora_stack_android/Stack.java
@@ -0,0 +1,13 @@
+package com.example.calculadora_stack_android;
+
+import java.util.List;
+
+public interface Stack<D> {
+    void empilhar(D dado);            // empilha o objeto "dado"
+    D desempilhar() throws Exception; // remove e retorna o objeto do topo
+    D oTopo() throws Exception;       // retorna o objeto do topo sem removê-lo
+    int getTamanho();                 // informa a quantidade de itens empilhados
+    boolean estaVazia();              // informa se a pilha está ou não vazia
+    List<D> dadosDaPilha();           // percorre a pilha e retorna uma lista com todos os itens empilhados,
+    // sem alterar o conteúdo da pilha
+}
